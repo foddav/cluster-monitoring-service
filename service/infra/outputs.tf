d6e9e74bc086762762e46d0570a84f4d6bc21651
Fodor Dávid <david.fodor96@gmail.com>
2025-09-10 10:53:05 +0200

    implement terraform files to create AWS EKS Cluster

---
+++ service/infra/outputs.tf
@@ -0,0 +1,11 @@
+output "cluster_name" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "kubeconfig_cmd" {
+  value = "aws eks update-kubeconfig --name ${module.eks.cluster_id} --region ${var.region}"
+}
