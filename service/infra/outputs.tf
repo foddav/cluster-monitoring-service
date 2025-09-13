f03e2a859bd27d27dc2b07257115b963504d9b4a
Fodor Dávid <david.fodor96@gmail.com>
2025-09-13 16:56:27 +0200

    tested service and fixed terraform and kubernetes files and updated readme

--- service/infra/outputs.tf
+++ service/infra/outputs.tf
@@ -5,8 +5,3 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
-
-output "kubeconfig_cmd" {
-  value = "aws eks update-kubeconfig --name ${module.eks.cluster_id} --region ${var.region}"
-}
-
